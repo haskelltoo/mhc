2cc2ff7e3bc52e4ee8c53d13f3ab6a768bc0bdf3
exclusive-and <exclusiveandgate@gmail.com>
2025-09-04 20:39:39 -0400

    Implemented slightly less invasive Nix scripts

--- code/mhc-haskell/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, mkDerivation, base } :
-
-mkDerivation {
-    pname = "mhc-haskell";
-    version = "0";
-    src = ./.;
-    libraryHaskellDepends = [ base ];
-    license = lib.licenses.bsd3;
-}
