f20f17fe45af9ca67975b6ec3ac797306add21ad
exclusive-and <exclusiveandgate@gmail.com>
2025-09-06 13:39:59 -0400

    Removed old nix scripts

--- nix/haskell-like/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, mkDerivation, base } :
-
-mkDerivation {
-    pname = "haskell-like";
-    version = "0";
-    src = ../../code/haskell-like;
-    libraryHaskellDepends = [ base ];
-    license = lib.licenses.bsd3;
-}
--- nix/language-lambek/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, mkDerivation, base } :
-
-mkDerivation {
-    pname = "language-lambek";
-    version = "0";
-    src = ../../code/language-lambek;
-    libraryHaskellDepends = [ base ];
-    license = lib.licenses.bsd3;
-}
